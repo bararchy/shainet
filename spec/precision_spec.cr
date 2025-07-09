32a78932da0a5b1c35cda948d2ac1ffc55992df0
Bar Hofesh <bar.hofesh@gmail.com>
2025-07-09 15:03:20 +0300

    Add half precision bindings and docs

--- spec/precision_spec.cr
+++ spec/precision_spec.cr
@@ -11,6 +11,16 @@ describe "Precision enum" do
     out.size.should eq(1)
   end
 
+  it "runs a network with bf16 precision" do
+    net = SHAInet::Network.new
+    net.precision = SHAInet::Precision::Bf16
+    net.add_layer(:input, 1, SHAInet.none)
+    net.add_layer(:output, 1, SHAInet.sigmoid)
+    net.fully_connect
+    out = net.run([0.25])
+    out.size.should eq(1)
+  end
+
   it "converts Float16 correctly" do
     h = SHAInet::Float16.new(1.5_f32)
     (h.to_f32 - 1.5_f32).abs.should be < 0.01
