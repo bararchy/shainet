28a045b39f0d6768261afa1690449d79237028a2
Bar Hofesh <bar.hofesh@gmail.com>
2025-07-09 12:44:48 +0300

    Add deterministic decoding specs

--- spec/decoding_spec.cr
+++ spec/decoding_spec.cr
@@ -2,14 +2,17 @@ require "./spec_helper"
 
 describe SHAInet do
   it "samples from top_k distribution" do
-    Random::DEFAULT.new_seed(42_u64, 54_u64)
-    logits = [-1.0, 0.0, 1.0, 2.0]
-    SHAInet.top_k_sample(logits, 2).should eq(2)
+    rng = Random::DEFAULT
+    rng.new_seed(1_u64, 1_u64)
+    logits = [0.1, 0.2, 0.3, 0.4]
+    res = SHAInet.top_k_sample(logits, 2, rng: rng)
+    [2, 3].includes?(res).should eq(true)
   end
 
   it "samples from top_p distribution" do
-    Random::DEFAULT.new_seed(42_u64, 54_u64)
-    logits = [-1.0, 0.0, 1.0, 2.0]
-    SHAInet.top_p_sample(logits, 0.7).should eq(2)
+    rng = Random::DEFAULT
+    rng.new_seed(1_u64, 1_u64)
+    logits = [0.5, 0.3, 0.1, 0.1]
+    SHAInet.top_p_sample(logits, 0.6, rng: rng).should eq(0)
   end
 end
