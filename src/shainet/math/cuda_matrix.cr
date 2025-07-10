7eefc845ca998ea317527e0110a95f2456418326
Bar Hofesh <bar.hofesh@gmail.com>
2025-07-10 20:31:59 +0300

    Fix GPU matrix conversion and disable problematic cuDNN path

--- src/shainet/math/cuda_matrix.cr
+++ src/shainet/math/cuda_matrix.cr
@@ -896,54 +896,8 @@ module SHAInet
         return self
       elsif @precision.in?({Precision::Fp16, Precision::Bf16, Precision::Fp32}) &&
             vec.precision == @precision && CUDNN.available?
-        {% if flag?(:enable_cuda) %}
-          begin
-            # Broadcast multiply using cuDNN OpTensor
-            op_desc = uninitialized LibCUDNN::CudnnOpTensorDescriptor
-            CUDNN.check_status(LibCUDNN.cudnnCreateOpTensorDescriptor(out op_desc))
-
-            begin
-              dtype = CUDNN.data_type_for(@precision)
-              CUDNN.check_status(LibCUDNN.cudnnSetOpTensorDescriptor(
-                op_desc,
-                LibCUDNN::CudnnOpTensorOp::CUDNN_OP_TENSOR_MUL,
-                dtype,
-                0))
-
-              mat_desc = CUDNN.create_tensor_descriptor_2d(@rows, @cols, @precision)
-              vec_desc = CUDNN.create_tensor_descriptor_2d(1, vec.cols, vec.precision)
-
-              alpha1 = 1.0
-              alpha2 = 1.0
-              beta = 0.0
-
-              self.sync_to_device!("mul_row_vector") unless device_dirty?
-              vec.sync_to_device!("mul_row_vector") unless vec.device_dirty?
-
-              CUDNN.check_status(LibCUDNN.cudnnOpTensor(
-                CUDNN.handle,
-                op_desc,
-                pointerof(alpha1).as(Pointer(Void)),
-                mat_desc,
-                dptr.as(Pointer(Void)),
-                pointerof(alpha2).as(Pointer(Void)),
-                vec_desc,
-                vptr.as(Pointer(Void)),
-                pointerof(beta).as(Pointer(Void)),
-                mat_desc,
-                dptr.as(Pointer(Void))
-              ))
-
-              mark_device_dirty!
-              return self
-            ensure
-              LibCUDNN.cudnnDestroyTensorDescriptor(mat_desc)
-              LibCUDNN.cudnnDestroyTensorDescriptor(vec_desc)
-            end
-          ensure
-            LibCUDNN.cudnnDestroyOpTensorDescriptor(op_desc)
-          end
-        {% end %}
+        # cuDNN optimized path disabled due to compilation issues.
+        # Fall back to CPU implementation below.
       end
 
       # CPU fallback
