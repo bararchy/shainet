4d9638a9658039f1fcce41b6ab085b83376cf78f
Bar Hofesh <bar.hofesh@gmail.com>
2025-07-15 12:33:38 +0300

    fix precision handling in safe_output_transform

--- spec/safe_output_transform_spec.cr
+++ spec/safe_output_transform_spec.cr
@@ -82,4 +82,33 @@ describe "safe_output_transform" do
       net.call_safe_output_transform(input, weights)
     end
   end
+
+  it "works when network precision is Fp16 and input is Fp32" do
+    pending! "CUDA not available" unless SHAInet::CUDA.available?
+
+    net = SHAInet::Network.new
+    net.precision = SHAInet::Precision::Fp16
+    net.hidden_layers << SHAInet::TransformerBlock.new(2, 1, 2)
+
+    input = SHAInet::CudaMatrix.new(2, 2, precision: SHAInet::Precision::Fp32)
+    weights = SHAInet::CudaMatrix.new(2, 1, precision: SHAInet::Precision::Fp16)
+
+    2.times do |i|
+      2.times do |j|
+        input[i, j] = (i * 2 + j + 1).to_f32
+      end
+    end
+    weights[0, 0] = 1.0_f32
+    weights[1, 0] = 2.0_f32
+
+    input.sync_to_device!
+    weights.sync_to_device!
+
+    output = net.call_safe_output_transform(input, weights)
+    output.sync_from_device!
+
+    output.rows.should eq 1
+    output.cols.should eq 1
+    output[0, 0].should be_close(input[1, 0]*1.0_f32 + input[1, 1]*2.0_f32, 1e-3_f32)
+  end
 end
